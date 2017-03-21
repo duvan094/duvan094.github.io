57b5b1c85dd4935409811b3798d81d0133aff4e1
Jacob <jacobduvander@gmail.com>
2017-03-21 19:38:04 +0100

    various small changes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 	$('a').click(function(){
 	    $('html, body').animate({
-	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-	    }, 500);
+	           scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 500);
 	    return false;
 	});
 
