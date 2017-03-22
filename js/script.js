dda53c41d81f434dceae5f3c38394d2b4018e54f
Jacob <jacobduvander@gmail.com>
2017-03-22 20:28:06 +0100

    added pages for each portfolio item + images

--- js/script.js
+++ js/script.js
@@ -7,6 +7,28 @@ $(document).ready(function(){
 	    return false;
 	});
 
+    if($(this).scrollTop()>=$('#nav-target').position().top-33){
+
+        var navLinks = document.querySelectorAll("nav>ul>li>a");
+
+        for(var i = 0; i<navLinks.length; i++){
+            navLinks[i].style.color = "#222";
+            navLinks[i].classList.remove("white-line");
+            navLinks[i].className = "black-line";
+        }
+
+    }else{
+        var navLinks = document.querySelectorAll("nav>ul>li>a");
+
+        for(var i = 0; i<navLinks.length; i++){
+            navLinks[i].style.color = "#FFF";
+            navLinks[i].classList.remove("black-line");
+            navLinks[i].className = "white-line";
+        }
+    }
+
+
+
 });
 
 
@@ -44,7 +66,6 @@ $(document).on('scroll', function() {
     	var backToTopButton = document.getElementById("back-to-top");
     	backToTopButton.className = "hideBTT";
     }
-
-
-
 });
+
+
