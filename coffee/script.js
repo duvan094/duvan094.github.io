ff59c85a576aeee84ca37d71fe533ec204b03cb3
Jacob <jacobduvander@gmail.com>
2018-03-11 22:12:18 +0100

    Added coffee project

---
+++ coffee/script.js
@@ -0,0 +1,27 @@
+console.log("test");
+/*document.getElementById("down-arrow").addEventListener("click",function(event){
+  console.log("test");
+});
+*/
+
+/*https://stackoverflow.com/questions/11213259/javascript-animation*/
+/*
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+var ghostHeader = document.getElementById("header-ghost");
+
+// Get the offset position of the navbar
+var stickyInitiator = document.getElementById("sticky");
+var sticky = stickyInitiator.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+if (window.pageYOffset >= sticky) {
+  header.classList.add("sticky");
+} else {
+  header.classList.remove("sticky");
+}
+}
+*/
